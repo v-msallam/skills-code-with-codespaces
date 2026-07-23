7356f6fb41c6af2d9a0cd4331f518bf10d1c5656
v-msallam <v-msallam@microsoft.com>
2026-07-23 05:09:06 +0000

    feat: Add features, extensions, and postCreate script

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,6 +3,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update -y
-sudo apt-get install -y sl
+sudo apt-get install -y sl git-lfs
+git lfs install --local
 echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
 echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
